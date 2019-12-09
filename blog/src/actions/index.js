d2774aa68c5c615911b593ce77d6edc72c6f2933
kiingdavies <daviesadedeji007@gmail.com>
2019-12-09 17:02:26 +0100

    Blog app DONE!!!

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,34 @@
-import jsonPlaceholader from "../apis/jsonPlaceholder";
+//Action creators that pass the action to the reducers
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  
+  await dispatch(fetchPosts());
+ //Using the chain function from lodash
+  _.chain(getState().posts)
+  .map('userId')
+  .uniq()
+  .forEach(id => dispatch(fetchUser(id)))
+  .value();
+};
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholader.get("/posts");
+  const response = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response.data });
+  dispatch({ type: "FETCH_POSTS", payload: response.data }); //The action is dispatched here
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data }); //The action is dispatched here
+}; //Using the memoize wont solve this same issue if you refactor your api, you would need to write this same code without the memoization
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data }); //The action is dispatched here
+// }); //Using the memoize wont solve this same issue if you refactor your api, you would need to write this same code without the memoization
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions";
-
+import { fetchPostsAndUsers } from "../actions";
+import UserHeader from './UserHeader';
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
+    // this.props.fetchPosts();
+  }
+
+  renderList() {
+    //Helper function to render the list in render() neatly
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId}/>
+          </div>
+          
+        </div>
+      );
+    });
   }
   render() {
-      console.log(this.props.posts);
-      
-    return <div>PostList</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
@@ -17,4 +35,4 @@ const mapStateToProps = state => {
   return { posts: state.posts };
 };
 
-export default connect(mapStateToProps, { fetchPosts })(PostList); //This is the react-redux
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList); //This is the react-redux
